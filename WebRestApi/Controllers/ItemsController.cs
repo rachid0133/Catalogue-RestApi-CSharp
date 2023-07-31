25d767ed24d825c1d4aa64114a619b37a79df037
Rachid Chakor - RACC2 <racc2@premiertech.com>
2023-07-31 09:08:45 -0400

    first commit created webApi in Memo

---
+++ WebRestApi/Controllers/ItemsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebRestApi.Entities;
+using WebRestApi.Repositories;
+
+namespace WebRestApi.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository _itemsRepository;
+
+        public ItemsController()
+        {
+            _itemsRepository = new InMemItemsRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = _itemsRepository.GetItems();
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = _itemsRepository.GetItem(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+    }
+}
---
+++ WebRestApi/Repositories/InMemItemsRepository.cs
@@ -0,0 +1,19 @@
+﻿using WebRestApi.Entities;
+
+namespace WebRestApi.Repositories
+{
+    public class InMemItemsRepository
+    {
+        private readonly List<Item> _items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name= "Potion", Price= 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name= "Iron Sword", Price= 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name= "Bronzw Sheild", Price= 18, CreatedDate = DateTimeOffset.UtcNow },
+
+        };
+
+        public IEnumerable<Item> GetItems() => _items;
+
+        public Item GetItem(Guid id) => _items.Where(i => i.Id==id).SingleOrDefault();
+    }
+}
